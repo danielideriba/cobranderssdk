99f043aab2876bc1b2afc3558adf8dfc7286b1f6
Daniel Ideriba <daniel.ideriba@quicard.com.br>
2018-05-16 15:06:12 -0300

    Welcome screen

---
+++ app/src/main/java/br/quigroup/com/cobranderssdk/views/MainActivity.java
@@ -0,0 +1,17 @@
+package br.quigroup.com.cobranderssdk.views;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import br.quigroup.com.cobranderssdk.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
