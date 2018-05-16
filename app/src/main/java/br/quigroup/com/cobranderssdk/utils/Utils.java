1db93c57ac8df5b4e8b5ff134f26d5d0579be01c
Daniel Ideriba <daniel.ideriba@quicard.com.br>
2018-05-16 14:43:05 -0300

    first commit

---
+++ app/src/main/java/br/quigroup/com/cobranderssdk/utils/Utils.java
@@ -0,0 +1,9 @@
+package br.quigroup.com.cobranderssdk.utils;
+
+import android.util.Log;
+
+public class Utils {
+    public static void customLog(String msg, String tag){
+        Log.i(tag, msg);
+    }
+}
